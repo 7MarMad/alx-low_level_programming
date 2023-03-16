0d25f371bca916f887d24dffeb8494c29fd1862e
Marwane Madani <marwane.madani99@gmail.com>
2023-03-16 16:05:25 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,12 @@
-#include<stdio.h
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Returning 1 (Success)
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
