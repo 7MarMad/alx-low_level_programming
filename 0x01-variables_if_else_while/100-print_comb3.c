738b7fb97c0155ae980cbfdca14baf0f9d80a45f
Marwane Madani <marwane.madani99@gmail.com>
2023-03-17 14:16:03 -0700

    fulfilling

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,29 +11,32 @@ int main(void)
 
 	for (i = 0; i < 90; i++)
 	{
-		putchar(i/10 + '0');
-		putchar(i%10 + '0');
-		if (i != 89)
+		if (i / 10 != i % 10)
 		{
-			putchar(',');
-                	putchar(' ');
-		}
-		if (i%10 == 9)
-		{
-			if (i/10 == 1)
-				i = i + 3;
-			else if (i/10 == 2)
-				i = i + 4;
-			else if (i/10 == 3)
-                                i = i + 5;
-			else if (i/10 == 4)
-                                i = i + 6;
-			else if (i/10 == 5)
-                                i = i + 7;
-			else if (i/10 == 6)
-                                i = i + 8;
-			else if (i/10 == 7)
-                                i = i + 9;
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			if (i != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			if (i % 10 == 9)
+			{
+				if (i / 10 == 1)
+					i = i + 3;
+				else if (i / 10 == 2)
+					i = i + 4;
+				else if (i / 10 == 3) 
+					i = i + 5;
+				else if (i / 10 == 4)
+					i = i + 6;
+				else if (i / 10 == 5)
+					i = i + 7;
+				else if (i / 10 == 6)
+					i = i + 8;
+				else if (i / 10 == 7)
+					i = i + 9;
+			}
 		}
 	}
 	putchar('\n');
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j > k)
+			{
+				for (i = 0; i < 10; i++)
+				{
+					if(i > j)
+					{
+						putchar(k + '0');
+						putchar(j + '0');
+						putchar(i + '0');
+						if (k != 7 || j != 8 || i!= 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
