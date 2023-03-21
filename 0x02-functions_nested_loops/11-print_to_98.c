0eedb2acc0f96f7aad220bf6fdddc57c3a03800e
Marwane Madani <marwane.madani99@gmail.com>
2023-03-21 15:13:05 -0700

    kkg

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,6 +8,8 @@
  */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n == 98)
 	{
 		_putchar(n / 10 + '0');
@@ -15,20 +17,20 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (n; n >= 98; n--)
+		for (i = n; i >= 98; i--)
 		{
-			if (n < 100)
+			if (i < 100)
 			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
 			}
 			else
 			{
-				_putchar(n / 100 + '0');
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
+				_putchar(i / 100 + '0');
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
 			}
-			if (n != 98)
+			if (i != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -37,11 +39,11 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n; n <= 98; n++)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
-			if (n != 98)
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			if (i != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
