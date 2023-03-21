93268ece9fcf8e78040895f4e359d4fcbb38b0ba
Marwane Madani <marwane.madani99@gmail.com>
2023-03-21 16:45:05 -0700

    jjf

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * times_table - another
+ */
+void times_table(int n)
+{
+	int i, j, h;
+
+	for (j = 0; j < (n + 1); j++)
+	{
+		for (i = 0; i < (n + 1); i++)
+		{
+			h = i * j;
+			if (i != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (h < (n + 1))
+					_putchar(' ');
+			}
+			if (h < (n + 1))
+			{
+				_putchar(h + '0');
+			}
+			else
+			{
+				_putchar(h / 10 + '0');
+				_putchar(h % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
