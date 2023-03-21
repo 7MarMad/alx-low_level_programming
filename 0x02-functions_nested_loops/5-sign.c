ad519f16772833b26350b7784a21c57076a4fa18
Marwane Madani <marwane.madani99@gmail.com>
2023-03-21 13:17:52 -0700

    mrr

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - printing
+ *
+ * @n: argumment
+ *
+ * Return: on number being + 1. being - -1 . being zero is 0
+ */
+int print_sign(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		i = -1;
+		_putchar('-');
+	}
+	else if (n == 0)
+	{
+		i = 0;
+		_putchar(0);
+	}
+	else{
+		i = 1;
+		_putchar('+');
+	}
+	return (i);
+}
