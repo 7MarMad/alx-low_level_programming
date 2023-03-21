04b7ca7fc6e0b1ad7a1430bd21e555a101bdbee5
Marwane Madani <marwane.madani99@gmail.com>
2023-03-21 11:40:53 -0700

    1-task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void print_alphabet(void);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
