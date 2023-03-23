512adb9c36cdbeda5a56f176d64a5ff9c2e6ec1a
Marwane Madani <marwane.madani99@gmail.com>
2023-03-23 09:59:41 -0700

    djk

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * print_square - square printing
+ * @size: arg
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
