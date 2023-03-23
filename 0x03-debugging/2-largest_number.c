4791b6c6e36aea051c5fb7eb4bad1bf7f67c4fff
Marwane Madani <marwane.madani99@gmail.com>
2023-03-23 00:19:55 -0700

    ff

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,11 +13,17 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if ((a > b && b > c) || (a > b && a > c))
+	{
 		largest = a;
+	}
 	else if ((b > a && a > c) || (b > a && b > c))
+	{
 		largest = b;
+	}
 	else
+	{
 		largest = c;
+	}
 
 	return (largest);
 }
