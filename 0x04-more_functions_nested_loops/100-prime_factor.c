17fd881f6d72304a990f70fb7a2d0b24c4275d34
Marwane Madani <marwane.madani99@gmail.com>
2023-03-23 12:56:06 -0700

    dle

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point for prime factors
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, max, a = 612852475143;
+
+	while (a % 2 == 0)
+	{
+		max = 2;
+		a = a / 2;
+	}
+
+	for (i = 3; i < sqrt(a); i = i + 2)
+	{
+		while (a % i == 0){
+			max = i;
+			a = a / 2;
+		}
+	}
+	if (n > 2)
+		max = a;
+	printf("%d\n", max);
+	return (0);
+}
