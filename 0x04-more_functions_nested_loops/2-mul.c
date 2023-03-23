b2fdfdfe8600027d54cb2885b8509e1771c6fa7b
Marwane Madani <marwane.madani99@gmail.com>
2023-03-23 08:55:10 -0700

    rg

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "main.h"
+
+/*
+ * mul - multiplication of 2 integers
+ * @a: arg
+ * @b: arg
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
