11e6606f6ff73a9cf57600f49afb3d607d416895
Marwane Madani <marwane.madani99@gmail.com>
2023-03-23 01:22:54 -0700

    ofp

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,16 +12,16 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b)
 	{
-		if (a > b && a > c)
+		if (b > c || a > c)
 		{
 			largest = a;
 		}
 	}
-	else if (b > a && a > c)
+	else if (b > a)
 	{
-		if (b > a && b > c)
+		if (a > c || b > c)
 		{
 			largest = b;
 		}
