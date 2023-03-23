22142f248b3a6d197993a9ac43997a24ad562beb
Marwane Madani <marwane.madani99@gmail.com>
2023-03-23 09:24:47 -0700

    6 & 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * more_numbers - getting far
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
