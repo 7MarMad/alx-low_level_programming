b0803a7b0bfa40b79d42138d27c95e049304ea7f
Marwane Madani <marwane.madani99@gmail.com>
2023-03-23 09:38:16 -0700

    if

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * print_diagonal - hj
+ * @n: arg
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (j = 0; j < n; j++)
+	{
+		for (i = 0; i < j ; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\');
+		_putchar('\n');
+	}
+}
