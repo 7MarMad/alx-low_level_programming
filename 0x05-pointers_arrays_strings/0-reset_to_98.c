ec2bf2a32722495caefd20a5f1c9e4d057f31f04
Marwane Madani <marwane.madani99@gmail.com>
2023-04-03 06:02:53 -0700

    rr

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - updating the value of what a pointer point to
  *
- *@*n: pointer as arg
+ *@n: pointer as arg
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,12 +2,13 @@
 
 /**
  * swap_int - swaping to integers
- * @*a: arg
- * @*b: arg
+ * @a: arg
+ * @b: arg
  */
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
