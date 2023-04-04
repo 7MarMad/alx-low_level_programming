4c67becaba41e3ba12276330dfc3e9287049ed9a
Marwane Madani <marwane.madani99@gmail.com>
2023-04-03 19:35:48 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i;
+	unsigned int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
-		printf("%s", a[8]);
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
 		printf("\n");
 	}
 }
