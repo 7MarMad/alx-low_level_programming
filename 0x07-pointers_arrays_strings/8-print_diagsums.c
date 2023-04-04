f68cfb5e59b48bfb0663803ba93b9320783ce68f
Marwane Madani <marwane.madani99@gmail.com>
2023-04-03 19:46:17 -0700

    digonal sum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * print_diagsums - sum of 2 digonals of a matrix
+ *
+ * @a: array
+ * @size: arrays size
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j = 0, k = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		j = j + a[i][i];
+		k = k + a[i][size - i - 1];
+	}
+	printf("%d, ", j);
+	printf("%d\n", k);
+}
