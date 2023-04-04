f49f9fa25c4a358f85898819356256cbf17cabc1
Marwane Madani <marwane.madani99@gmail.com>
2023-04-04 07:34:04 -0700

    oo

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * set_string - setting a pointer to another value
+ *
+ * @s: arg
+ * @to: arg
+ */
+
+void set_string(char **s, char *to)
+{
+	int i;
+
+	for (i = 0; to[i] != '\0'; i++)
+	{
+		*s[i] = to[i];
+	}
+}
