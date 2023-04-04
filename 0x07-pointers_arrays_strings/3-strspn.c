03313d2dc92fe11bfa29e8aa5fe0ab0d6a2a588e
Marwane Madani <marwane.madani99@gmail.com>
2023-04-03 18:14:05 -0700

    ll

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * _strspn - calculating the maching characters
+ * @s: arg
+ * @accept: arg
+ *
+ * Return: the number of bytes in the initial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0, i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				len++;
+				break;
+			}
+		}
+		if (len == 0)
+			break;
+	}
+	return (len);
+}
