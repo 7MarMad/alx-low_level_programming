d4a439836dcc5cb006e213b656cde72f8455fe7e
Marwane Madani <marwane.madani99@gmail.com>
2023-04-05 15:28:56 -0700

    warning

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 
 /**
+ * _sqrt_recursion - finding the natural square root
+ * @n: arg
+ * Return: result or -1 as not there
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	return (_sqrt_recursion2(n, i));
+}
+/**
  * _sqrt_recursion2 - finding the natural square root of a number
  * @n: the number we are working on
  * @i: the guess result
@@ -20,17 +32,4 @@ int _sqrt_recursion2(int n, int i)
 	{
 		return (-1);
 	}
-}
-
-/**
- * _sqrt_recursion - finding the natural square root
- * @n: arg
- * Return: result or -1 as not there
- */
-
-int _sqrt_recursion(int n)
-{
-	int i = 0;
-
-	return (_sqrt_recursion2(n, i));
 }
