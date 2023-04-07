6b641e2ecd5772e9cea14ff6770a2d6f04ec47cc
Marwane Madani <marwane.madani99@gmail.com>
2023-04-06 18:08:35 -0700

    wildcmp function

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,7 +13,7 @@ int wildcmp(char *s1, char *s2)
 	else if (s1[0] == s2[0])
 		return (wildcmp(s1 + 1, s2 + 1));
 	else if (s2[0] == '*')
-		return ((s[0] != '\0' && wildcmp(s1 + 1, s2)) || wildcmp(s1, s2 + 1));
+		return ((s1[0] != '\0' && wildcmp(s1 + 1, s2)) || wildcmp(s1, s2 + 1));
 	else
 		return (0);
 }
