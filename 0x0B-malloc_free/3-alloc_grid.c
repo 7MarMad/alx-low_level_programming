c317cb5494bea4e44ef539d65d6cf203102af547
Marwane Madani <marwane.madani99@gmail.com>
2023-04-11 16:26:08 -0700

    pp

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,15 +14,22 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ar = (int**)malloc(width * sizeof(int*));
+	ar = (int **)malloc(height * sizeof(int *));
 	if (ar == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		ar[i] = (int*)malloc(height * sizeof(int));
+		ar[i] = (int *)malloc(width * sizeof(int));
 		if (ar[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(ar[i--]);
+			}
+			free(ar);
 			return (NULL);
-		for (j = 0; j < height; j++)
+		}
+		for (j = 0; j < width; j++)
 		{
 			ar[i][j] = 0;
 		}
--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -38,15 +38,15 @@ int main(void)
 {
     int **grid;
 
-    grid = alloc_grid(35, 42);
+    grid = alloc_grid(48, 32);
     if (grid == NULL)
     {
         return (1);
     }
-    print_grid(grid, 35, 42);
+    print_grid(grid, 48, 32);
     printf("\n");
     grid[0][3] = 98;
     grid[3][4] = 402;
-    print_grid(grid, 35, 42);
+    print_grid(grid, 48, 32);
     return (0);
 }
