d8f114237eb2b5d89c42591e596ac4f1307920d4
Marwane Madani <marwane.madani99@gmail.com>
2023-04-11 17:59:22 -0700

    oo

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
@@ -10,28 +11,28 @@
 char *argstostr(int ac, char **av)
 {
 	char *s;
-	int len = 0, i, cur_len, prev_len = 0;
+	int len = 0, i, ac2, cur_len, prev_len = 0;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
-	else
+	ac2 = ac;
+	while (ac2 > 0)
 	{
-		while (ac > 0)
-		{
-			len = len + strlen(av[--ac]);
-		}
-		s = malloc(((len + ac) * sizeof(char)) + 1);
-		if (s == NULL)
-			return (NULL);
-		for (i = 0; i < ac; i++)
-		{
-			cur_len = strlen(av[i]);
-			memcpy(s + prev_len, av[i], cur_len);
-			prev_len += cur_len;
-			s[prev_len++] = '\n';
-		}
+		len = len + strlen(av[--ac2]);
+	}
+	s = malloc(((len + ac) * sizeof(char)) + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		cur_len = strlen(av[i]);
+		memcpy(s + prev_len, av[i], cur_len);
+		prev_len += cur_len;
+		s[prev_len++] = '\n';
 	}
 	return (s);
 }
