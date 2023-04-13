84a2ea8e70ed797f076121b942435fcf47c2043d
Marwane Madani <marwane.madani99@gmail.com>
2023-04-13 16:39:46 -0700

    ll

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,6 +20,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	if (new_size == 0 && ptr == NULL)
+		return (NULL);
 	if (ptr == NULL)
 	{
 		arr = malloc(new_size * sizeof(void *));
