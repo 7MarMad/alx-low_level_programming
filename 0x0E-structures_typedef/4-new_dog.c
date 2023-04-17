36faadab394fee5d9c86c63db93651fa4648f705
Marwane Madani <marwane.madani99@gmail.com>
2023-04-17 11:09:39 -0700

    rt

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include<stdlib.h>
+#include<string.h>
 
 /**
  * new_dog - creting a new instance of a structre
@@ -10,14 +11,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t nn;
-	dog_t *new;
-
-	new = &nn;
+	dog_t *new_dog;
+	int len1, len2;
+	
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	new->name = name;
-	new->age = age;
-	new->owner = owner;
-	return (new);
+	len1 = strlen(name);
+	len2 = strlen(owner);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = malloc((sizeof(char) * len1) + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = malloc((sizeof(char) * len2) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog);
+		free(new_dog->name);
+		return (NULL);
+	}
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_ dog);
 }
