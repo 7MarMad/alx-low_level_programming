2e09b2bc49b106ded6d11c6fcafcf230ad7dd4c3
Marwane Madani <marwane.madani99@gmail.com>
2023-04-17 17:00:48 -0700

    preprocessor & macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - printing the name of the file
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	exit(EXIT_SUCCESS);
+}
