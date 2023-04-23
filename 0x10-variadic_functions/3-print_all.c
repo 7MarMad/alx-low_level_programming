1603bf002199bddd590d0565a299242a3426c7c6
Marwane Madani <marwane.madani99@gmail.com>
2023-04-23 08:43:20 -0700

    fix multiple if

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,7 +7,7 @@
  */
 void print_all(const char * const format, ...)
 {
-	int count = 0, coun = 0;
+	int count = 0, coun = 0, track = 1;
 	const char *p, *pp;
 	va_list list;
 
@@ -22,34 +22,31 @@ void print_all(const char * const format, ...)
 	va_start(list, format);
 	while (*p != '\0')
 	{
+		if (coun != count && coun == track)
+		{
+			printf(", ");
+			track++;
+		}
 		switch (p[0])
 		{
 			case 'i':
 				coun++;
 				printf("%d", va_arg(list, int));
-				if (coun != count)
-					printf(", ");
 				p++;
 				continue;
 			case 'c':
 				coun++;
 				printf("%c", va_arg(list, int));
-				if (coun != count)
-					printf(", ");
 				p++;
 				continue;
 			case 'f':
 				coun++;
 				printf("%f", va_arg(list, double));
-				if (coun != count)
-					printf(", ");
 				p++;
 				continue;
 			case 's':
 				coun++;
 				printf("%s", va_arg(list, char*));
-				if (coun != count)
-					printf(", ");
 				p++;
 				continue;
 		}
