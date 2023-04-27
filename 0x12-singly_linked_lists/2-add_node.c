7cc7ebcb15a6e31f14ac7d4f4aa9c191e1e191b1
Marwane Madani <marwane.madani99@gmail.com>
2023-04-27 07:58:10 -0700

    fix task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,7 +5,7 @@
 /**
  * add_node - adding a new node to the linked list
  *
- * @h: current head of the linked list
+ * @head: current head of the linked list
  * @str: a string to be duplicated in one if the data types in the structre
  *
  * Return: pointer to the new node created
@@ -21,7 +21,7 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->len = strlen(str);
 	new_node->next = NULL;
 	if (*head != NULL)
-		new_node->next = *head ;
+		new_node->next = *head;
 	*head = new_node;
 	return (new_node);
 }
