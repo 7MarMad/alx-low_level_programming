5fbc653f75564ac835c4de025d6622b433164b9b
Marwane Madani <marwane.madani99@gmail.com>
2023-04-27 12:32:48 -0700

    getting started with assembly

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * startup_func - executing the function before the main
+ *
+ */
+void startup_func(void) __attribute__ ((constructor));
+
+void startup_func(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
