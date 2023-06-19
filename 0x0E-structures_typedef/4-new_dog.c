1a380122a8ca46d55907e962d7090d8032d0be2d
marmad <marwane.madani99@gmail.com>
2023-06-19 11:21:59 -0700

    making more mallocs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creating a new dog struct
@@ -11,15 +12,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-/*	dog_t new_d;*/
 	dog_t *new;
+	int len1 = 0, len2 = 0;
 
+	if (name != NULL)
+		len1 = strlen(name);
+	if (owner != NULL)
+		len2 = strlen(owner);
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
-
+	new->name = malloc((sizeof(char) * len1) + 1);
+	if (new->name == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->name = name;
 	new->age = age;
+	new->owner = malloc((sizeof(char) * len2) + 1);
+	if (new->owner == NULL)
+	{
+		free(new->name);
+		free(new);
+		return (NULL);
+	}
 	new->owner = owner;
 	return (new);
 }
