fa9274687e99430922add3da79a4bf29721efe76
marmad <marwane.madani99@gmail.com>
2023-06-19 11:26:36 -0700

    again

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,7 +28,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new);
 		return (NULL);
 	}
-	new->name = name;
+	strcpy(new->name, name);
 	new->age = age;
 	new->owner = malloc((sizeof(char) * len2) + 1);
 	if (new->owner == NULL)
@@ -37,6 +37,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new);
 		return (NULL);
 	}
-	new->owner = owner;
+	strcpy(new->owner, owner);
 	return (new);
 }
