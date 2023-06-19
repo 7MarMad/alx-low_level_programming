47375fee50bed592f895960e57053a40f079cb1f
marmad <marwane.madani99@gmail.com>
2023-06-19 07:26:50 -0700

    all tasks of pre-processor project

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - printing the name of the file using standard macro
+ *
+ * Return: 0 for Success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
