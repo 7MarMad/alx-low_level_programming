7643f6d6b2a1b8f3af7325a18dccf29243937020
marmad <marwane.madani99@gmail.com>
2023-06-22 08:44:25 -0700

    trying another version

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,54 +1,86 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include<stdio.h>
+#include<stdarg.h>
 
 /**
- * print_all - printing all the strings passed on depending on the format
- * @format: a string containing a character helping in knowing the type of arg
+ * counting - counting the parameters of the string
+ * @p: the string
+ * Return: result
+ */
+int counting(const char *p)
+{
+	int s = 0;
+
+	while (*p != '\0')
+	{
+		switch (p[0])
+		{
+			case 'c':
+				s++;
+				p++;
+				continue;
+			case 'i':
+				s++;
+				p++;
+				continue;
+			case 'f':
+				s++;
+				p++;
+				continue;
+			case 's':
+				s++;
+				p++;
+				continue;
+		}
+		p++;
+	}
+	return (s);
+}
+
+/**
+ * print_all - printing all arguments nomatter what are there types
+ * @format: the string to get parameters from
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	const char *s;
-	char *ss, *com = ""; /*sl[] = {'c', 'i', 'f', 's'};*/
-	int i = 0;
+	int count = 0, coun = 0, track = 1;
+	const char *p, *pp;
+	va_list list;
 
-	if (format == NULL)
-		return;
-	s = format;
-	va_start(args, format);
-	while (s[i] != '\0')
+	pp = format;
+	p = format;
+	count = counting(pp);
+	va_start(list, format);
+	while (*p != '\0')
 	{
-/*		j = 0;*/
-		switch (s[i])
+		if (coun != count && coun == track)
 		{
-			case 'c':
-				printf("%s%c", com, va_arg(args, int));
-				break;
+			printf(", ");
+			track++;
+		}
+		switch (p[0])
+		{
 			case 'i':
-				printf("%s%d", com, va_arg(args, int));
-				break;
+				coun++;
+				printf("%d", va_arg(list, int));
+				p++;
+				continue;
+			case 'c':
+				coun++;
+				printf("%c", va_arg(list, int));
+				p++;
+				continue;
 			case 'f':
-				printf("%s%f", com, va_arg(args, double));
-				break;
+				coun++;
+				printf("%f", va_arg(list, double));
+				p++;
+				continue;
 			case 's':
-				ss = va_arg(args, char *);
-				if (ss == NULL)
-					ss = "(nil)";				
-				printf("%s%s", com, ss);
-				break;
-			default:
-				i++;
+				coun++;
+				printf("%s", va_arg(list, char*));
+				p++;
 				continue;
 		}
-/*		while (j < 4)
-		{
-			if (s[i + 1] == sl[j])
-			{
-				printf(", "), j = 4;
-			} j++;
-		}*/
-		com = ", ";
-		i++;
+		p++;
 	}
-	printf("\n"), va_end(args);
-}
+	printf("\n");
+	va_end(list);
