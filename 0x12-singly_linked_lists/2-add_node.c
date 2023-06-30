9be96bfa475d1b73b3747463750996ba39de9038
marmad <marwane.madani99@gmail.com>
2023-06-30 07:32:06 -0700

    not using strlen anymore

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,26 @@
 #include <string.h>
 
 /**
+ * str_len - length of string
+ * @str: string to work on
+ *
+ * Return: length of the string
+ */
+int str_len(const char *str)
+{
+        int len = 0;
+
+        if (str == NULL)
+                return (-1);
+        while (str[0] != '\0')
+        {
+                len++;
+                str++;
+        }
+        return (len);
+}
+
+/**
  * add_node - adding a node to the begining if the linked list
  * @head: current head if linked list
  * @str: string to be duplicated in the node made
@@ -12,14 +32,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	int lenn;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	if (str != NULL)
+	lenn = str_len(str);
+	if (lenn != -1)
 	{
 		new->str = strdup(str);
-		new->len = strlen(new->str);
+		new->len = lenn;
 	}
 	else
 		new->str = NULL;
